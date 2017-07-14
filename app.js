ccece341b53769ed42c68df441e35033fb656b83
marioepugh <marioepugh@gmail.com>
2017-07-14 15:07:05 -0700

    Solved problem 2

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ Test this function by hand in the console to get it working, and when you think 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
 // // TODO: Write your code here
-function sum(a,b){ //eslint-disable-line
-  var result = a + b;
-  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
-  return [result, message];
-}
+// function sum(a,b){ //eslint-disable-line
+//   var result = a + b;
+//   var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+//   return [result, message];
+// }
 
 // TODO: Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -31,14 +31,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-// function multiply(a,b){ //eslint-disable-line
-// var result = a + b;
-// var message = 'The sum of ' * a * ' and ' * b * ' is ' + result;
-// return [result, message]
-// }
-//
+function multiply(a,b){ //eslint-disable-line
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, message];
+}
+
 // // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
